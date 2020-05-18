f13048a3d7566c43d73ec2596a8f31f3b692088f
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-05-18 16:58:48 +0200

    Nice

---
+++ submit.php
@@ -0,0 +1,18 @@
+<?php
+$firstnumber = filter_input(INPUT_POST, 'firstnumber');
+$secondnumber = filter_input(INPUT_POST, 'secondnumber');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+ <li> <?= $firstnumber ?> + <?= $secondnumber ?> = <?= $firstnumber + $secondnumber ?>
+
+</ul>    
+</body>
+</html>
